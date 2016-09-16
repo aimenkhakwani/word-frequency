f8632e8583350d8a09bffe986dc72a3ad5c1d930
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-09-16 10:05:31 -0700

    Add user interface with twig pages

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Sentence.php';
+
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use($app){
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->get('/output', function() use($app){
+        $new_Sentence = new Sentence;
+        $result = $new_Sentence->evaluateWords($_GET['input1'], $_GET['input2']);
+        return $app['twig']->render('output.html.twig', array('result' => $result));
+    });
+
+    return $app;
+?>
